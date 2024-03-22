5f0791f5f0308b34a85a625118e6f109cf602e79
Isfar Babul <isfarb2@nycstudents.net>
2024-03-22 01:07:42 -0400

    flare up complete by Isfar

--- src/FlareUp.java
+++ src/FlareUp.java
@@ -13,6 +13,29 @@ public class FlareUp extends FireCard {
         do {
             targettedSpace = getBoard().getSpace(getScan());
         } while (targettedSpace instanceof Fire || targettedSpace instanceof Firebreak);
-
+        getBoard().obtainBoard()[targettedSpace.getY()][targettedSpace.getX()] = new Fire(targettedSpace.getX(), targettedSpace.getY());
+        String choice = "";
+        while (!choice.equals("NORTH") && !choice.equals("EAST") && !choice.equals("SOUTH") && !choice.equals("WEST")) {
+            System.out.print("In which direction would you like to build your flare in(NORTH, EAST, SOUTH, WEST)? ");
+            choice = getScan().nextLine();
+        }
+        WindDirection targetWind;
+        if (choice.equalsIgnoreCase("NORTH")) {
+            targetWind = WindDirection.NORTH;
+        } else if (choice.equalsIgnoreCase("SOUTH")) {
+            targetWind = WindDirection.SOUTH;
+        } else if (choice.equalsIgnoreCase("EAST")) {
+            targetWind = WindDirection.EAST;
+        } else {
+            targetWind = WindDirection.WEST;
+        }
+        Space adjacentSpace = getBoard().checkOrthogonallyAdjacent(targettedSpace, targetWind);
+        if (!(adjacentSpace instanceof Fire) && !(adjacentSpace instanceof Firebreak)) {
+            getBoard().obtainBoard()[adjacentSpace.getY()][adjacentSpace.getX()] = new Fire(adjacentSpace.getX(), adjacentSpace.getY());
+            Space nextAdjacentSpace = getBoard().checkOrthogonallyAdjacent(targettedSpace, targetWind);
+            if (!(nextAdjacentSpace instanceof Fire) && !(nextAdjacentSpace instanceof Firebreak)) {
+                getBoard().obtainBoard()[nextAdjacentSpace.getY()][nextAdjacentSpace.getX()] = new Fire(nextAdjacentSpace.getX(), nextAdjacentSpace.getY());
+            }
+        }
     }
 }
