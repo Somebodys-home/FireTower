67297353fe98f1d0aeb03e115e3838ae2bd9fa2a
Gabriel <hypixelnerd347@gmail.com>
2024-03-21 22:14:35 -0400

    Please no trouble Especially not double - Gabriel & Isfar

--- src/FlareUp.java
+++ src/FlareUp.java
@@ -9,11 +9,10 @@ public class FlareUp extends FireCard {
     public void cardEffect() {
         System.out.println("Select a space on the board that isn't part of the eternal flame or a fire tower area.");
         System.out.println("This space must build off of an existing fire space.");
-        System.out.print("Select an x-coordinate: ");
-        int x = getScan().nextInt();
-        getScan().nextLine();
-        System.out.print("Select a y-coordinate: ");
-        int y = getScan().nextInt();
-        getScan().nextLine();
+        Space targettedSpace;
+        do {
+            targettedSpace = getBoard().getSpace(getScan());
+        } while (targettedSpace instanceof Fire || targettedSpace instanceof Firebreak);
+
     }
 }
