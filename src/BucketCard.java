fba348864610d794620c47fab34568da4dadc934
Atif Ali_ <aatif1771@gmail.com_>
2024-03-23 22:35:10 -0400

    Made some progress on the BucketCard class. -Atif Ali

--- src/BucketCard.java
+++ src/BucketCard.java
@@ -9,6 +9,18 @@ public class BucketCard extends Card {
 
     @Override
     public void cardEffect() {
-
+        if (hasWater) {
+            System.out.println("Select a fire space to extinguish.");
+            Space targetSpace = getBoard().getSpace(getScan());
+            if (targetSpace instanceof Fire) {
+                targetSpace = new Space("🌲", targetSpace.getX(), targetSpace.getY()); // replace fire with tree
+                hasWater = false; // bucket is now empty
+                System.out.println("Fire extinguished!");
+            } else {
+                System.out.println("Invalid target. Please select a fire space.");
+            }
+        } else {
+            System.out.println("Bucket is empty. Cannot extinguish fire.");
+        }
     }
 }
