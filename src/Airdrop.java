6af46296378b42cceed82dcf4c181bb872f5b5c2
Atif Ali_ <aatif1771@gmail.com_>
2024-03-25 19:39:49 -0400

    Did some work on the AirDrop class -Atif Ali

--- src/Airdrop.java
+++ src/Airdrop.java
@@ -7,6 +7,16 @@ public class Airdrop extends WaterCard{
 
     @Override
     public void cardEffect() {
-
+        System.out.println("Select up to three fire spaces to extinguish.");
+        for (int i = 0; i < 3; i++) {
+            Space targetSpace = getBoard().getSpace(getScan());
+            if (targetSpace instanceof Fire) {
+                targetSpace = new Space("🌲", targetSpace.getX(), targetSpace.getY()); // replace fire with tree
+                System.out.println("Fire extinguished at (" + targetSpace.getX() + ", " + targetSpace.getY() + ")!");
+            } else {
+                System.out.println("Invalid target. Please select a fire space.");
+                i--; // decrement i to retry this iteration
+            }
+        }
     }
 }
