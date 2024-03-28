604ab76c0febf4181a75ecc5af2d8135d8c8f599
Isfar Babul <isfarb2@nycstudents.net>
2024-03-28 01:19:50 -0400

    Changes by babula(boogey man)

--- src/FireTowerGame.java
+++ src/FireTowerGame.java
@@ -27,34 +27,39 @@ public class FireTowerGame {
         turnOrder = new Player[playerNum];
         ArrayList<String> hearts = new ArrayList<String>();
         hearts.add("❤️");
-        hearts.add("\uD83D\uDC9C");
-        hearts.add("\uD83D\uDC99");
-        hearts.add("\uD83D\uDC9A");
+        hearts.add("💜");
+        hearts.add("💙");
+        hearts.add("💚");
         String choice;
         do {
             System.out.println("The first player may choose from the available hearts to defend(copy paste the heart to choose it): " + hearts);
             choice = scan.nextLine();
         } while (!hearts.contains(choice));
+        turnOrder[0] = new Player("...", board, new BucketCard(board, scan), 0, 0); //TODO: HIGHLIGHTED
         if (turnOrder.length == 2) {
             int otherChoice = hearts.indexOf(choice) + 2;
             if (otherChoice >= hearts.size()) {
                 otherChoice -= hearts.size();
             }
             System.out.println("The other player must now defend " + hearts.get(otherChoice));
+            turnOrder[1] = new Player("...", board, new BucketCard(board, scan), 0, 0); //TODO: HIGHLIGHTED
         } else {
             hearts.remove(choice);
             do {
                 System.out.println("The second player may choose from the available hearts to defend(copy paste the heart to choose it): " + hearts);
                 choice = scan.nextLine();
             } while (!hearts.contains(choice));
+            turnOrder[1] = new Player("...", board, new BucketCard(board, scan), 0, 0); //TODO: HIGHLIGHTED
             hearts.remove(choice);
             do {
                 System.out.println("The third player may choose from the available hearts to defend(copy paste the heart to choose it): " + hearts);
                 choice = scan.nextLine();
             } while (!hearts.contains(choice));
+            turnOrder[2] = new Player("...", board, new BucketCard(board, scan), 0, 0); //TODO: HIGHLIGHTED
             if (hearts.size() == 4) {
                 hearts.remove(choice);
                 System.out.println("The fourth player must now defend " + hearts.get(0));
+                turnOrder[3] = new Player("...", board, new BucketCard(board, scan), 0, 0); //TODO: HIGHLIGHTED
             }
         }
     }
